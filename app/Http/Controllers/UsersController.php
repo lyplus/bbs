bddd23d361ed375059fbdbc227748a2f8bf7ec4b
lyplus <sly99088@gmali.com>
2017-11-17 01:02:27 +0800

    Upload avatar

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,9 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
+
 class UsersController extends Controller
 {
     public function show(User $user){
         return view('users.show',compact('user'));
+    }
+
+    public function edit(User $user){
+        return view('users.edit',compact('user'));
+
+    }
+
+    public function update(UserRequest $request,ImageUploadHandler $uploader,User $user){
+        $data=$request->all();
+        if($request->avatar){
+            $result=$uploader->save($request->avatar,'avatar',$user->id,360);
+            if($result){
+                $data['avatar']=$result['path'];
+            }
+        }
+
+        $user->update($data);
+        return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功');
     }
 }
---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Auth;
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'=>'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
+            'email'=>'required|email',
+            'introduction'=>'max:80',
+            'avatar'=>'mimes:jpg,jpeg,bmp,png,gif|dimensions:min_width=200,min_height=200',
+        ];
+    }
+    public function messages(){
+        return[
+            'name.required'=>'用户名不能为空',
+            'name.regex'=>'用户名只支持中英文、数字、横杆和下划线',
+            'name.between'=>'用户名必须介于 3 - 25 个字符之间',
+            'name.unique'=>'名字已被占用',
+            'avatar.mimes' => '头像必须是一个jpg, jpeg, bmp, png, gif 类型的文件',
+            'avatar.dimensions' => '图片的清晰度不够，宽和高需要 200px 以上',
+        ];
+    }
+}
