d2970f2470958f55f49da0b3e133684593983d18
lyplus <sly99088@gmali.com>
2017-11-18 01:11:52 +0800

    Topic order

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -8,8 +8,9 @@ use App\Models\Category;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category){
-        $topics=Topic::where('category_id',$category->id)->paginate(30);
-        return view('topics.index',compact('topics','category'));
+    public function show(Category $category,Request $request,Topic $topic)
+    {
+        $topics=Topic::withOrder($request->order)->where('category_id', $category->id)->paginate(30);
+        return view('topics.index', compact('topics', 'category'));
     }
 }
