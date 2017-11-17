9bd338f8cffc5a6cac1e7d7febb2feb5ac23cf92
lyplus <sly99088@gmali.com>
2017-11-17 23:59:57 +0800

    Topic index page

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,4 +5,11 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
 }
