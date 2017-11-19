663a3e116c28a0f1cf3d7650d4feb6bd64ae47a6
lyplus <sly99088@gmali.com>
2017-11-19 15:18:10 +0800

    Author can delete topic

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,6 +27,10 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    // public function isAuthorOf($model){
+    //     return $this->id==$model->user_id;
+    // }
+
     public function topics(){
         return $this->hasMany(Topic::class);
     }
